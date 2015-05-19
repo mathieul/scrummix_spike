369988cd0e338dabfe6ae01d2c42a0901fc1e9a6
Mathieu Lajugie <mathieu@caring.com>
2015-05-18 19:47:33 -0700

    test using channels

--- web/static/js/apps/daily-edit.js
+++ web/static/js/apps/daily-edit.js
@@ -1,7 +1,14 @@
+import {Socket} from "phoenix";
 import SectionEditor from '../components/section-editor';
 import SectionStore from '../stores/section';
 /* global React */
 /* global Reflux */
+
+let socket = new Socket("/ws");
+socket.connect();
+let channel = socket.join("sections:lobby", {})
+channel.receive("ok", chan => console.log("welcome to chat:", chan));
+channel.push("hello there", {body: "something here"});
 
 let Application = React.createClass({
   mixins: [Reflux.connect(SectionStore, 'section')],
